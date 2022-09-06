c83e6273ef4817ac9afa99fa744827c227c0823d
Edinsoncs <edinsoncode@gmail.com>
2022-09-06 23:49:21 +0300

    fix

--- src/pages/Home/feed.js
+++ src/pages/Home/feed.js
@@ -38,7 +38,19 @@ export default class Feed extends Component {
                             <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
                             <VolumeMenuButton true />
                         </ControlBar>
-                    </Player>
+            </Player>
+            <ul className='listFeed'>
+              <li>
+                <a href='https://www.iproup.com/economia-digital/33721-asi-funciona-ryoshi-la-nueva-plataforma-para-inversores-cripto' target='_blank'>
+                How does Ryoshi work, the new platform developed for crypto investors?
+                </a>
+              </li>
+              <li>
+                <a href='https://www.diariobitcoin.com/notas-de-prensa/conozca-ryoshi-la-herramienta-para-tener-a-la-mano-todos-los-datos-sobre-defi-en-un-solo-lugar/' target='_blank'>
+                Get to know RYOSHI, the tool to have all the data about DeFi at hand in one place
+                </a>
+              </li>
+            </ul>
           </article>
         </div>
 
