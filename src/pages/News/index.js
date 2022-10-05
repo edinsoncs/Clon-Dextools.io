e869c033393a9d18b60d3d767bd4e9a437680d2d
Edinson <edinson@MacBook-Pro-de-Edinson.local>
2022-10-05 15:31:48 +0300

    upfx

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -16,92 +16,81 @@ function News() {
   useEffect(() => {
 
     fetch('https://api.metadex.tools/news')
-    .then(response => response.json())
-    .then(data => setNews(data))
+      .then(response => response.json())
+      .then(data => setNews(data))
 
   }, []);
 
-  function viewNews(id){
+  function viewNews(id) {
     setView(news[id]);
-    return function(){
+    return function () {
 
     }
 
   }
 
-  if(news.length){
+  if (news.length) {
+    console.log('news', news);
     defaultNews.title = news[0].title;
     defaultNews.content = news[0].content;
     defaultNews.link = news[0].link;
   }
 
   return (
-  <>
-    <Sidebar />
+    <>
+      <Sidebar />
 
-    <main>
+      <main>
 
-      <Header />
+        <Header />
 
-      <div className="content">
+        <div className="content">
 
-         <Subheader />
+          <Subheader />
 
-         <section className="board page-news">
-             <div className="board-col col1">
-                 <div className="board-block tools-suni">
-                     <div className="col-title"><span className="icon"></span> LAST <strong>NEWS</strong><img className="line" src="images/line.svg" /></div>
-                     <div className="col-content">
-                         <div class="line-list-news">
-
-                         {news.map((e, i, a) => <>
-                           <a class="item-news" href="#" onClick={viewNews.bind(this, i)}>
-                             <span class="content-details"><b>{i+1}</b> {e.title}</span>
-                           </a>
-                           </>)}
-
-
-                         </div>
-
-                     </div>
-                 </div>
-             </div>
-
-             <div className="board-col col2-news">
-                 <div className="board-block tools-suni">
-                     <div className="col-title"><span className="icon"></span> PREVIEW <strong>NEWS</strong><img className="line" src="images/line.svg" /></div>
-                     <div className="col-content">
-                         <div class="line-preview">
-
-                            <div className='title-view-news'>
-                                <h2>{(Object.keys(view).length) ? view.title : defaultNews.title}</h2>
-                            </div>
-
-                            <div className='description-view-news'  dangerouslySetInnerHTML={{__html: (Object.keys(view).length) ? view.content : defaultNews.content}}>
-                            </div>
-
-                            <div className='visit-view-news'>
+          <section className="board page-news">
+            <div className="board-cols-news">
+              <div className="board-block news-ryoshi">
+                <div className="col-title"><span className="icon"></span> LAST <strong>NEWS</strong><img className="line" src="images/line.svg" /></div>
+                <section className='full-view-news'>
+                    <div className='title-view-news'>
+                      <h2>{(Object.keys(view).length) ? view.title : defaultNews.title}</h2>
+                    </div>
+                    <div className='description-view-news' dangerouslySetInnerHTML={{ __html: (Object.keys(view).length) ? view.content : defaultNews.content }}>
+                    </div>
+                    <div className='visit-view-news'>
                               <a href={ (Object.keys(view).length) ? view.link : defaultNews.link } className='link-visit' target='_blank'>
                                 See the full news
                               </a>
-                            </div>
+                    </div>
+                  </section>
 
-                         </div>
+                <div className="col-content">
+                  <div class="line-list-news">
 
-                     </div>
-                 </div>
+                    {news.map((e, i, a) => <>
+                      <a class="item-news" href="#" onClick={viewNews.bind(this, i)}>
+                        <span class="content-position"><b>{i + 1}</b> </span>
+                        <span class="content-details">{e.title}</span>
+                      </a>
+                    </>)}
+                  </div>
 
-             </div>
+                </div>
+              </div>
+            </div>
 
-         </section>
 
-      </div>
 
-      <Footer/>
+          </section>
 
-    </main>
+        </div>
 
-  </>
+        <Footer />
+
+      </main>
+
+    </>
   );
 }
 
